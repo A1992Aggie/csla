bf99792e67423767e5701b7cc077805c329940e5
Rockford Lhotka <rocky@lhotka.net>
2018-04-29 21:39:13 -0500

    #854 Use PropertyInfo in Xamarin project.

---
+++ Samples/ProjectTracker/ProjectTracker.Ui.Xamarin/ProjectTracker.Ui.Xamarin/Xaml/ErrorColor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ProjectTracker.Ui.Xamarin.Xaml
+{
+    public class ErrorColor : IValueConverter
+    {
+    public bool Invert { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      var condition = (bool)value;
+      if (Invert) condition = !condition;
+      if (condition)
+        return "Black";
+      else
+        return "Red";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      return value;
+    }
+  }
+}
